f521a2e796349ed6f5a29627e545dc92d45d034c
smt19f <smt19f@fsu.edu>
2023-12-11 22:08:25 -0600

    added models for the rest of the items

---
+++ src/main/java/com/github/poopertools/item/custom/CopperHoe.java
@@ -0,0 +1,13 @@
+package com.github.poopertools.item.custom;
+
+import net.minecraft.world.item.HoeItem;
+import net.minecraft.world.item.Tier;
+
+public class CopperHoe extends HoeItem {
+
+    public CopperHoe(Tier p_41336_, int p_41337_, float p_41338_, Properties p_41339_) {
+        super(p_41336_, p_41337_, p_41338_, p_41339_);
+    }
+
+
+}
